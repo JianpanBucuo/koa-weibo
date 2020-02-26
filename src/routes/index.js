b467f7b89093df7d733eecbbd71339beee3f8f9d
jinjian <jinj116@chinaunicom.cn>
2020-02-26 14:49:02 +0800

    feat ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,25 @@
 module.exports =  (router) => {
   router.get('/welcome', async function (ctx, next) {
-    ctx.state = {
-      title: 'koa2 title'
-    };
+    // ctx.state = {
+    //   title: 'koa2 ',
+    //   des:' Des'
+    // };
 
-    await ctx.render('welcome', {title: ctx.state});
+
+    await ctx.render('welcome', {
+                title: 'koa2',
+                des: '1234',
+                isMe: false,
+                blogList:[
+                  {
+                    id:1,
+                    title: 'aaa'
+                  },
+                  {
+                    id:2,
+                    title:'bbb'
+                  }
+                ]
+              });
   })
 }
